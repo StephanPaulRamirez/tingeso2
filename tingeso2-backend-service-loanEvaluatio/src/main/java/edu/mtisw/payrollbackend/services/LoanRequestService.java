3a0bdd2fe8b0ab49c7cc4fa576a41b80e7f67d51
Stephan Paul <stephan.paul@usach.cl>
2024-12-05 14:46:46 -0300

    cambios para mejorar logica microservicios
    cambios en evaluation

--- tingeso2-backend-service-loanEvaluatio/src/main/java/edu/mtisw/payrollbackend/services/LoanRequestService.java
+++ tingeso2-backend-service-loanEvaluatio/src/main/java/edu/mtisw/payrollbackend/services/LoanRequestService.java
@@ -1,28 +1,45 @@
 package edu.mtisw.payrollbackend.services;
 
 import edu.mtisw.payrollbackend.entities.LoanRequestEntity;
-import edu.mtisw.payrollbackend.repositories.LoanRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
 @Service
 public class LoanRequestService {
-    @Autowired
-    LoanRequestRepository loanRequestRepository;
 
+    @Autowired
+    RestTemplate restTemplate;
 
     public LoanRequestEntity getLoanRequestById(Long id){
-        return loanRequestRepository.findById(id).get();
+        ResponseEntity<LoanRequestEntity> loanRequest = restTemplate.getForEntity(
+                "http://followCredit-service/api/v1/loanTracking/id/"+ id, LoanRequestEntity.class);
+        return loanRequest.getBody();
     }
 
     public ArrayList<LoanRequestEntity> getLoanRequestByUserRut(String rut){
-        return loanRequestRepository.findByUserRut(rut);
+        return restTemplate.exchange(
+                "http://followCredit-service/api/v1/loanTracking/rut/" + rut,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<ArrayList<LoanRequestEntity>>() {}
+        ).getBody();
     }
 
     public LoanRequestEntity updateLoanRequest(LoanRequestEntity loanRequest) {
-        return loanRequestRepository.save(loanRequest);
+        ResponseEntity<LoanRequestEntity> response = restTemplate.exchange(
+                "http://followCredit-service/api/v1/loanTracking/",
+                HttpMethod.PUT,
+                new HttpEntity<>(loanRequest),
+                LoanRequestEntity.class
+        );
+        return response.getBody();
     }
 
 }
